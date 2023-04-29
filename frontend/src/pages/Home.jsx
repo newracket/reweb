448c790f8e85568bb02604ee14efd75c34178213
newracket <newracket@users.noreply.github.com>
2023-04-28 22:32:47 -0700

    Fixed centering image bug

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Navbar from "../components/Navbar.jsx";
-import Sidebar from "../components/Sidebar.jsx";
 import java from "../assets/javalogo.svg";
 import python from "../assets/pythonlogo.svg";
 import cpp from "../assets/cpplogo.svg";
@@ -8,32 +7,28 @@ import "../styles/home.css";
 function Home() {
   return (
     <div className="home-outer-container">
-      <Navbar />
+      <Navbar/>
       <div className="home-container">
         <div className="home-banner">
           <div className="banner-text">
-            <h1>
-              ReWeb
-            </h1>
-            <p>
-              Say goodbye to messy code!
-            </p>
+            <h1>ReWeb</h1>
+            <p>Say goodbye to messy code!</p>
           </div>
         </div>
       </div>
-      <div className = "upload-section">
-        <button class="upload-button" role="button">
+      <div className="upload-section">
+        <button className="upload-button" role="button">
           Choose Files
         </button>
       </div>
-      <div className = "supported-languages">
-        <h1>
-          What languages do we support?
-        </h1>
-        <div className = "logo-container">
+      <div className="supported-languages">
+        <h1>What languages do we support?</h1>
+        <div className="logo-container">
           <img src={java} alt="Java Logo" className="java-logo"/>
           <img src={python} alt="Python Logo" className="python-logo"/>
-          <img src={cpp} alt="CPP Logo" className="cpp-logo"/>
+          <div className="cpp-logo-box">
+            <img src={cpp} alt="CPP Logo" className="cpp-logo"/>
+          </div>
         </div>
       </div>
     </div>
