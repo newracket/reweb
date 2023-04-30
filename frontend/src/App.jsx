49a14065625b2edde3d519c337f4c66f3d8e6bdd
jsuh0020 <jsuh0020@gmail.com>
2023-04-29 20:26:28 -0700

    added about page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import "./styles/app.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Changes from "./pages/Changes.jsx";
+import About from "./pages/About.jsx";
 
 function App() {
   // Add router here
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/changes" element={<Changes />} />
+        <Route path="/about" element={<About />} />
       </Routes>
     </BrowserRouter>
   );
