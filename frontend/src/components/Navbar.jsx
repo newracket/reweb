954812124e3155befc24f72c004dc5ab5bcbf864
newracket <newracket@users.noreply.github.com>
2023-04-29 20:31:33 -0700

    Updated navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -10,6 +10,15 @@ function Navbar() {
         <img src="./reweb.svg" alt="ReWeb Logo" className="logo" />
         <h1 className="title">ReWeb</h1>
       </div>
+
+      <div className="navLinks">
+        <div className="navLink" onClick={() => navigate("/")}>
+          Home
+        </div>
+        <div className="navLink" onClick={() => navigate("/about")}>
+          About
+        </div>
+      </div>
     </nav>
   );
 }
